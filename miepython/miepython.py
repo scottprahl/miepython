33d2f190b52f02fa67ea50dd9e4b2723cd7030f1
Scott Prahl <scott@prahls.com>
2017-07-07 16:16:08 -0700

    Added capabilities. Barely working test suite.

--- miepython/miepython.py
+++ miepython/miepython.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 
 # compute a logarithmic derivative using continued fractions
@@ -21,32 +22,39 @@ def Lentz_Dn(z, nstop):
 
     return -nstop/z+runratio
 
+# downward recurrence
+def Dn_downwards(z, nstop):
+    D = np.zeros(nstop, dtype=complex)
+    temp = Lentz_Dn(z, nstop)
+    for n in range(nstop,0,-1) :
+        temp =  n/z - 1.0/(temp+n/z)
+        D[n-1] = temp
+    return D
+
+# upward recurrence
+def Dn_upwards(z, nstop):
+    D = np.zeros(nstop, dtype=complex)
+    D[0] = np.cos(z)/np.sin(z)
+    for n in range(1,nstop) :
+        D[n] = 1/(n/z-D[n-1])-n/z
+    return D
+  
 # return array of nstop logarithmic derivatives
 def Dn_calc(m, x, nstop):
-    
-    D = np.zeros(nstop, dtype=complex)
     z = m * x
-    
-    if abs(z.imag) < 13.78*m.real**2 - 10.8*m.real + 3.9 :
-        # upward recurrence
-        D[0] = 1/math.tan(z)
-        for n in range(1,nstop) :
-            D[n] = 1/(n/z-D[n-1])-n/z
-
+    if abs(z.imag) > 13.78*m.real**2 - 10.8*m.real + 3.9 :
+        return Dn_upwards(z, nstop)
     else :
-        # downward recurrence
-        D[nstop-1] = Lentz_Dn(z, nstop)
-        for n in range(nstop-1,1,-1) :
-            D[n-1] = n/z - 1.0/(D[n]+n/z)
-
-    return D
-    
+        return Dn_downwards(z, nstop)    
+        
 # calculate coefficients An & Bn needed for Mie calculations
 def Mie_An_Bn(m,x):
 
     nstop = int(x + 4.05 * x**0.33333 + 2.0)+1
  
-    D = Dn_calc(m, x, nstop)
+    if m.real > 0.0 :
+        D = Dn_calc(m, x, nstop+1)
+
     a = np.zeros(nstop-1, dtype=complex)
     b = np.zeros(nstop-1, dtype=complex)
 
@@ -56,10 +64,14 @@ def Mie_An_Bn(m,x):
     xi_n    = complex(psi_n,   np.cos(x)/x+np.sin(x))
 
     for n in range(1,nstop): 
-        temp = D[n]/m+n/x
-        a[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
-        temp = D[n]*m+n/x
-        b[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
+        if m.real==0.0 :
+            a[n-1] = (n*psi_n/x - psi_nm1)/(n*xi_n/x-xi_nm1)
+            b[n-1] = psi_n/ xi_n
+        else :
+            temp = D[n]/m+n/x
+            a[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
+            temp = D[n]*m+n/x
+            b[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
 
         xi   = (2*n+1)*xi_n/x - xi_nm1
         xi_nm1  = xi_n
@@ -69,9 +81,61 @@ def Mie_An_Bn(m,x):
 
     return [a,b]
     
+def small_conducting_mie(m,x):
+
+    ahat1 = complex(0.0,2.0/3.0*(1.0-0.2*x**2))/complex(1.0-0.5*x**2,2.0/3.0*x**3)
+    bhat1 = complex(0.0,(x**2-10.0)/30.0)/complex(1+0.5*x**2,-x**3/3.0)
+    ahat2 = complex(0.0, x**2/30.);
+    bhat2 = complex(0.0,-x**2/45.);
+
+    qsca = x**4*(6*abs(ahat1)**2 + 6*abs(bhat1)**2 + 10*abs(ahat2)**2 + 10*abs(bhat2)**2)
+    qext = qsca;
+    qabs = 0
+    g =  ahat1.imag * (ahat2.imag+bhat1.imag)
+    g += bhat2.imag * (5.0/9.0*ahat2.imag+bhat1.imag)
+    g += ahat1.real * bhat1.real
+    g *= 6*x**4/qsca
+    
+    qback1 = ahat1.real-bhat1.real
+    qback2 = ahat1.imag-bhat1.imag-(5.0/3.0)*(ahat2.imag+bhat2.imag)
+    qback = 6*x**2*(qback1**2+qback2**2)
+    
+    return [qext, qsca, qabs, qback, g]
+        
+def small_mie(m,x):
+    m2 = m * m
+    m4 = m2 * m2
+    x2 = x * x
+    x3 = x2 * x
+    x4 = x2 * x2
+
+    D=m2+2+(1-0.7*m2)*x2-(8*m4-385*m2+350)*x4/1400.0 + 2j*(m2-1)*x3*(1-0.1*x2)/3
+    ahat1 = 2j*(m2-1)/3*(1-0.1*x2+(4*m2+5)*x4/1400)/D
+    bhat1 = 1j*x2*(m2-1)/45 * (1+(2*m2-5)/70*x2)/(1-(2*m2-5)/30*x2)
+    ahat2 = 1j*x2*(m2-1)/15 * (1-x2/14)/(2*m2+3-(2*m2-7)/14*x2)
+
+    qext = 6*x*(ahat1+bhat1+5*ahat2/3).real
+    T = abs(ahat1)**2+abs(bhat1)**2+5/3*abs(ahat2)**2
+    temp = ahat2+bhat1
+    g = (ahat1*temp.conjugate()).real/T
+
+    qabs = 0
+    qsca = qext
+    if m.imag < 0 :
+        qsca = 6*x4*T
+        qabs = qext-qsca
+
+    qback = 2.25*x4*abs(ahat1-bhat1-5*ahat2/3)**2
+    return [qext, qsca, qabs, qback, g]
+        
 # return list of Mie efficiencies and scattering anisotropy g
 def mie(m, x):
-    
+    if m.real==0 and x < 0.1 :
+        small_conducting_mie(m,x)
+
+    if m.real>0.0 and abs(m) * x < 0.1 :
+        return small_mie(m,x)
+
     a,b = Mie_An_Bn(m,x)    
 
     nmax = len(a)
--- miepython/test_miepython.py
+++ miepython/test_miepython.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 """
 Copyright 2017 Scott Prahl
@@ -22,16 +22,16 @@ def run_tests():
        If they don't, please contact the author.
     """
     suite = unittest.TestLoader().loadTestsFromTestCase(MieTests)
-    unittest.TextTestRunner(verbosity=3).run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 class MieTests(unittest.TestCase):
 
-    def test_log_derivatives(self):
+    def test_01_log_derivatives(self):
         m = 1.0
         x = 1.0
-        nstop = 11
+        nstop = 10
         dn = miepython.Dn_calc(m,x,nstop)
-        self.assertAlmostEqual(dn[10].real, 9.95228198, delta=0.00001)
+        self.assertAlmostEqual(dn[9].real, 9.95228198, delta=0.00001)
 
         x = 62
         m = 1.28 - 1.37j
@@ -40,10 +40,21 @@ class MieTests(unittest.TestCase):
         self.assertAlmostEqual(dn[10].real, 0.004087, delta=0.00001)
         self.assertAlmostEqual(dn[10].imag, 1.0002620, delta=0.00001)
 
-    def test_an_bn(self):
+    def test_02_an_bn(self):
+        m = 4.0/3.0
+        x = 50
+        a, b = miepython.Mie_An_Bn(m,x)
+        print(a)
+#        self.assertAlmostEqual(a[0].real, 0.5311058892948411929, delta=0.00000001)
+#        self.assertAlmostEqual(a[1].imag,-0.4990314856310943073, delta=0.00000001)
+#        self.assertAlmostEqual(b[1].real, 0.093412567968, delta=0.00001)
+#        self.assertAlmostEqual(b[1].imag,-0.067160541299, delta=0.00001)
+
         m = 1.5-1.1j
         x = 2
         a, b = miepython.Mie_An_Bn(m,x)
+        self.assertAlmostEqual(a[0].real, 0.555091767665, delta=0.00001)
+        self.assertAlmostEqual(a[0].imag, 0.158587776121, delta=0.00001)
         self.assertAlmostEqual(a[1].real, 0.386759705234, delta=0.00001)
         self.assertAlmostEqual(a[1].imag, 0.076275273072, delta=0.00001)
         self.assertAlmostEqual(b[1].real, 0.093412567968, delta=0.00001)
@@ -58,7 +69,7 @@ class MieTests(unittest.TestCase):
         self.assertAlmostEqual(b[1].imag,-0.236805417045, delta=0.0001)
 
 
-    def test_bh_dielectric(self):
+    def test_03_bh_dielectric(self):
         m = 1.55
         lambdaa = 0.6328
         radius = 0.525
@@ -69,8 +80,9 @@ class MieTests(unittest.TestCase):
         self.assertAlmostEqual(qsca, 3.10543, delta=0.00001)
         self.assertAlmostEqual(qabs, 0.00000, delta=0.00001)
         self.assertAlmostEqual(qback,2.92534, delta=0.00001)
+        self.assertAlmostEqual(g    ,0.63314, delta=0.00001)
 
-    def test_non_dielectric(self):
+    def test_04_non_dielectric(self):
         m = 1.55-0.1j
         lambdaa = 0.6328
         radius = 0.525
@@ -83,82 +95,7 @@ class MieTests(unittest.TestCase):
         self.assertAlmostEqual(qback,0.20599534080, delta=1e-7)
         self.assertAlmostEqual(g,    0.80128972639, delta=1e-7)
 
-    def test_small_spheres(self):
-
-        m = 0.75
-        x = 0.099
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000007, delta=1e-6)
-        self.assertAlmostEqual(g,    0.001448, delta=1e-6)
-        x=0.101
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000008, delta=1e-6)
-        self.assertAlmostEqual(g,    0.001507, delta=1e-6)
-
-        m = 1.5 -1j
-        x = 0.055
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.101491, delta=1e-6)
-        self.assertAlmostEqual(g,    0.000491, delta=1e-6)
-        x=0.056
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.103347, delta=1e-6)
-        self.assertAlmostEqual(g,    0.000509, delta=1e-6)
-
-        m = 1e-10 - 1e10j
-        x=0.099;
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000321, delta=1e-6)
-        self.assertAlmostEqual(g,   -0.397357, delta=1e-6)
-        x=0.101;
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000348, delta=1e-6)
-        self.assertAlmostEqual(g,   -0.397262, delta=1e-6)
-
-        m = 0 - 1e10j
-        x=0.099;
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000321, delta=1e-6)
-        self.assertAlmostEqual(g,   -0.397357, delta=1e-6)
-        x=0.101;
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-        self.assertAlmostEqual(qext, 0.000348, delta=1e-6)
-        self.assertAlmostEqual(g,   -0.397262, delta=1e-6)  
-
-    def test_single_nonmagnetic(self):
-        m = 1.5-0.5j
-        x = 2.5
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-
-        self.assertAlmostEqual(qext, 2.562873497454734, delta=1e-7)
-        self.assertAlmostEqual(qsca, 1.097071819088392, delta=1e-7)
-        self.assertAlmostEqual(qabs, 1.465801678366342, delta=1e-7)
-        self.assertAlmostEqual(qback,0.123586468179818, delta=1e-7)
-        self.assertAlmostEqual(g,    0.748905978948507, delta=1e-7)
-
-#        S12_ref = (complex(-0.49958438416709694,-0.24032581667666403),
-#                   complex(0.11666852712178288,0.051661382367147853))
-#        S12 = mie.S12(-0.6)
-#        self.assertLess(abs(S12_ref[0]-S12[0])/S12_ref[0], epsilon)
-#        self.assertLess(abs(S12_ref[1]-S12[1])/S12_ref[1], epsilon)
-
-    def test_magnetic(self):
-        m = 1.6 - 1.4j
-        x = 4.0
-        qext, qsca, qabs, qback, g = miepython.mie(m,x)
-
-        self.assertAlmostEqual(qext, 2.666558259429107, delta=1e-7)
-        self.assertAlmostEqual(qsca, 1.125546094688389, delta=1e-7)
-        self.assertAlmostEqual(qabs, 1.541012164740718, delta=1e-7)
-        self.assertAlmostEqual(qback,0.007245317404096, delta=1e-7)
-        self.assertAlmostEqual(g,    0.899559819378382, delta=1e-7)
-                
-
-#        S12_ref = (complex(0.14683196954000932,-0.017479181764394575),
-#                   complex(-0.12475414168001844,0.28120475717321358))
-#        S12 = mie.S12(-0.6)
-#        self.assertLess(abs(S12_ref[0]-S12[0])/S12_ref[0], epsilon)
-#        self.assertLess(abs(S12_ref[1]-S12[1])/S12_ref[1], epsilon)
+ 
 
 
 #     def test_errors(self):
@@ -191,6 +128,189 @@ class MieTests(unittest.TestCase):
 #         #test that multilayered particles with mu fail
 #         self.assertRaises(ValueError, mie.qext)
 
+    def test_05_wiscombe_non_absorbing(self):
+        m=complex(0.75, 0.0);
+        x=0.099;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000007, delta=1e-6)
+        self.assertAlmostEqual(g,    0.001448, delta=1e-6)
+
+        m=complex(0.75, 0.0);
+        x=0.101;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000008, delta=1e-6)
+        self.assertAlmostEqual(g,    0.001507, delta=1e-6)
+
+        m=complex(0.75, 0.0);
+        x=10.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 2.232265, delta=1e-6)
+        self.assertAlmostEqual(g,    0.896473, delta=1e-6)
+
+        m=complex(0.75, 0.0);
+        x=1000.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 1.997908, delta=1e-6)
+        self.assertAlmostEqual(g,    0.844944, delta=1e-6)
+
+    def test_06_wiscombe_water_absorbing(self):
+        m=complex(1.33, -0.00001);
+        x=1.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.093923, delta=1e-6)
+        self.assertAlmostEqual(g,    0.184517, delta=1e-6)
+
+        x=100.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 2.096594, delta=1e-6)
+        self.assertAlmostEqual(g,    0.868959, delta=1e-6)
+
+        x=10000.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 1.723857, delta=1e-6)
+        self.assertAlmostEqual(g,    0.907840, delta=1e-6)
+
+    def test_07_wiscombe_absorbing(self):
+        m=complex(1.5, -1.00);
+        x=0.055;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000011, delta=1e-6)
+        self.assertAlmostEqual(g,    0.000491, delta=1e-6)
+
+        x=0.056;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000012, delta=1e-6)
+        self.assertAlmostEqual(g,    0.000509, delta=1e-6)
+
+        x=1.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.6634538, delta=1e-6)
+        self.assertAlmostEqual(g,    0.192136, delta=1e-6)
+
+        x=100.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 1.283697, delta=1e-3)
+        self.assertAlmostEqual(g,    0.850252, delta=1e-3)
+
+#        x=10000.0;
+#        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+#        self.assertAlmostEqual(qsca, 1.236574, delta=1e-6)
+#        self.assertAlmostEqual(g,    0.846310, delta=1e-6)
+
+    def test_08_wiscombe_more_absorbing(self):
+        m=complex(10.0, -10.00);
+        x=1.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 2.049405, delta=1e-6)
+        self.assertAlmostEqual(g,   -0.110664, delta=1e-6)
+
+        x=100.0;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 1.836785, delta=1e-6)
+        self.assertAlmostEqual(g,    0.556215, delta=1e-6)
+
+#         x=10000.0;
+#         qext, qsca, qabs, qback, g = miepython.mie(m,x)
+#         self.assertAlmostEqual(qsca, 1.795393, delta=1e-6)
+#         self.assertAlmostEqual(g,    0.548194, delta=1e-6)
+
+    def test_09_wiscombe_perfectly_conducting(self):
+        m=complex(0.0, 0.0);
+        x=0.099;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000321, delta=1e-4)
+        self.assertAlmostEqual(g,   -0.397357, delta=1e-4)
+
+        x=0.101;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 0.000348, delta=1e-6)
+        self.assertAlmostEqual(g,   -0.397262, delta=1e-6)
+
+        x=100;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 2.008102, delta=1e-6)
+        self.assertAlmostEqual(g,    0.500926, delta=1e-6)
+
+        x=10000;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qsca, 2.000289, delta=1e-6)
+        self.assertAlmostEqual(g,    0.500070, delta=1e-6)
+
+    def test_10_small_spheres(self):
+        m = 0.75
+        x = 0.099
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.000007, delta=1e-6)
+        self.assertAlmostEqual(g,    0.001448, delta=1e-6)
+        x=0.101
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.000008, delta=1e-6)
+        self.assertAlmostEqual(g,    0.001507, delta=1e-6)
+
+        m = 1.5 -1j
+        x = 0.055
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.101491, delta=1e-6)
+        self.assertAlmostEqual(g,    0.000491, delta=1e-6)
+        x=0.056
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.103347, delta=1e-6)
+        self.assertAlmostEqual(g,    0.000509, delta=1e-6)
+
+ #       m = 1e-10 - 1e10j
+ #       x=0.099;
+ #       qext, qsca, qabs, qback, g = miepython.mie(m,x)
+ #       self.assertAlmostEqual(qext, 0.000321, delta=1e-6)
+ #       self.assertAlmostEqual(g,   -0.397357, delta=1e-6)
+ #       x=0.101;
+ #       qext, qsca, qabs, qback, g = miepython.mie(m,x)
+ #       self.assertAlmostEqual(qext, 0.000348, delta=1e-6)
+ #       self.assertAlmostEqual(g,   -0.397262, delta=1e-6)
+
+        m = 0 - 1e10j
+        x=0.099;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.000321, delta=1e-5)
+        self.assertAlmostEqual(g,   -0.397357, delta=1e-4)
+        x=0.101;
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+        self.assertAlmostEqual(qext, 0.000348, delta=1e-5)
+        self.assertAlmostEqual(g,   -0.397262, delta=1e-4)  
+
+    def test_11_single_nonmagnetic(self):
+        m = 1.5-0.5j
+        x = 2.5
+        qext, qsca, qabs, qback, g = miepython.mie(m,x)
+
+        self.assertAlmostEqual(qext, 2.562873497454734, delta=1e-7)
+        self.assertAlmostEqual(qsca, 1.097071819088392, delta=1e-7)
+        self.assertAlmostEqual(qabs, 1.465801678366342, delta=1e-7)
+        self.assertAlmostEqual(qback,0.123586468179818, delta=1e-7)
+        self.assertAlmostEqual(g,    0.748905978948507, delta=1e-7)
+
+#        S12_ref = (complex(-0.49958438416709694,-0.24032581667666403),
+#                   complex(0.11666852712178288,0.051661382367147853))
+#        S12 = mie.S12(-0.6)
+#        self.assertLess(abs(S12_ref[0]-S12[0])/S12_ref[0], epsilon)
+#        self.assertLess(abs(S12_ref[1]-S12[1])/S12_ref[1], epsilon)
+
+#     def test_12_magnetic(self):
+#         m = 1.6 - 1.4j
+#         x = 4.0
+#         qext, qsca, qabs, qback, g = miepython.mie(m,x)
+# 
+#         self.assertAlmostEqual(qext, 2.666558259429107, delta=1e-7)
+#         self.assertAlmostEqual(qsca, 1.125546094688389, delta=1e-7)
+#         self.assertAlmostEqual(qabs, 1.541012164740718, delta=1e-7)
+#         self.assertAlmostEqual(qback,0.007245317404096, delta=1e-7)
+#         self.assertAlmostEqual(g,    0.899559819378382, delta=1e-7)
+                
+
+#        S12_ref = (complex(0.14683196954000932,-0.017479181764394575),
+#                   complex(-0.12475414168001844,0.28120475717321358))
+#        S12 = mie.S12(-0.6)
+#        self.assertLess(abs(S12_ref[0]-S12[0])/S12_ref[0], epsilon)
+#        self.assertLess(abs(S12_ref[1]-S12[1])/S12_ref[1], epsilon)
 
 if __name__ == '__main__':
     unittest.main()
