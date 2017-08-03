64cd6b9a492a3d8c860f4aaa2b5b7faca4ca45f7
Work Prahl <prahl@bme.ogi.edu>
2017-08-03 12:51:01 -0700

    add functions to __init__.py

--- setup.py
+++ setup.py
@@ -29,4 +29,21 @@ setup(
 	keywords=['mie', 'scattering', 'rainbow', 'droplet'],
 	install_requires=['numpy'],
 	test_suite='miepython.test.test',
+	long_description=
+	"""
+	When a plane wave encounters a perfect sphere then some of the light will
+	be absorbed and some will be scattered.  Mie developed the equations that
+	describe the scattered light wave.  These equations are complicated and 
+	involve infinite sums of Bessel functions.  Not surprisingly, calculating
+	the scattered profiles is complicated and easily gotten wrong.  Fortunately
+	Wiscombe identified the challenges and implemented these in Fortran code.
+	
+	This code is pure python and uses many of the ideas that Wiscombe developed.
+	However, it is *not* as accurate (especially for large spheres with size parameters
+	larger than 100).
+	
+	This code provides python functions for calculating the extinction efficiency,
+	scattering efficiency, backscattering, and scattering asymmetry.  Moreover, a
+	set of angles can be given and the scattering will be calculated for each angle.
+	""",
 )
