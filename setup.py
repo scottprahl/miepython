e15e393864b91fe94d8a61aac24a3f798bc90da4
Scott Prahl <scott@prahls.com>
2018-03-03 13:47:47 -0800

    update version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017 Scott Prahl
+Copyright 2018 Scott Prahl
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -13,7 +13,7 @@ from setuptools import setup
 setup(
 	name='miepython',
 	packages=['miepython'],
-	version='1.0.0',
+	version='1.1.0',
 	description='Mie scattering of a plane wave by a sphere',
 	url='https://github.com/scottprahl/miepython.git',  
 	author='Scott Prahl',
