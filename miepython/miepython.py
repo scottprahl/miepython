635aacbe315e95dfab95575c361d10c0b053047b
Scott Prahl <scott@prahls.com>
2018-06-24 20:47:47 -0700

    pep8 compliance and delinting using pylint

--- miepython/miepython.py
+++ miepython/miepython.py
@@ -2,16 +2,16 @@ from __future__ import division
 
 import numpy as np
 
-__all__ = [  'generate_mie_costheta',
-             'i_par',
-             'i_per',
-             'i_unpolarized',
-             'mie',
-             'mie_S1_S2',
-             'mie_cdf',
-             'mie_mu_with_uniform_cdf'
-             ]
-      
+__all__ = ['generate_mie_costheta',
+           'i_par',
+           'i_per',
+           'i_unpolarized',
+           'mie',
+           'mie_S1_S2',
+           'mie_cdf',
+           'mie_mu_with_uniform_cdf']
+
+
 def _Lentz_Dn(z, N):
     """ Compute the logarithmic derivative of the Ricatti-Bessel function
 
@@ -19,23 +19,23 @@ def _Lentz_Dn(z, N):
         using the continued fraction technique of Lentz, Appl. Opt., 15,
         668-671, (1976).
     """
-    zinv     =  2.0/z
-    alpha    =  (N+0.5) * zinv
-    aj       = -(N+1.5) * zinv
-    alpha_j1 = aj+1/alpha
+    zinv = 2.0 / z
+    alpha = (N + 0.5) * zinv
+    aj = -(N + 1.5) * zinv
+    alpha_j1 = aj + 1 / alpha
     alpha_j2 = aj
-    ratio    = alpha_j1/alpha_j2
-    runratio = alpha*ratio
+    ratio = alpha_j1 / alpha_j2
+    runratio = alpha * ratio
 
-    while abs(abs(ratio)-1.0) > 1e-12:
+    while abs(abs(ratio) - 1.0) > 1e-12:
         aj = zinv - aj
-        alpha_j1 = 1.0/alpha_j1 + aj
-        alpha_j2 = 1.0/alpha_j2 + aj
-        ratio = alpha_j1/alpha_j2
-        zinv  *= -1
-        runratio = ratio*runratio
+        alpha_j1 = 1.0 / alpha_j1 + aj
+        alpha_j2 = 1.0 / alpha_j2 + aj
+        ratio = alpha_j1 / alpha_j2
+        zinv *= -1
+        runratio = ratio * runratio
 
-    return -N/z+runratio
+    return -N / z + runratio
 
 
 def _D_downwards(z, N):
@@ -47,8 +47,8 @@ def _D_downwards(z, N):
     D = np.zeros(N, dtype=complex)
     last_D = _Lentz_Dn(z, N)
     for n in range(N, 0, -1):
-        last_D =  n/z - 1.0/(last_D+n/z)
-        D[n-1] = last_D
+        last_D = n / z - 1.0 / (last_D + n / z)
+        D[n - 1] = last_D
     return D
 
 
@@ -59,10 +59,10 @@ def _D_upwards(z, N):
         argument z using the upwards recurrence relations.
     """
     D = np.zeros(N, dtype=complex)
-    exp = np.exp(-2j*z)
-    D[1] = -1/z + (1-exp)/((1-exp)/z-1j*(1+exp))
+    exp = np.exp(-2j * z)
+    D[1] = -1 / z + (1 - exp) / ((1 - exp) / z - 1j * (1 + exp))
     for n in range(2, N):
-        D[n] = 1/(n/z-D[n-1])-n/z
+        D[n] = 1 / (n / z - D[n - 1]) - n / z
     return D
 
 
@@ -71,7 +71,7 @@ def _D_calc(m, x, N):
         orders (from 0 to N) with argument z
     """
     z = m * x
-    if abs(z.imag) > 13.78*m.real**2 - 10.8*m.real + 3.9:
+    if abs(z.imag) > 13.78 * m.real**2 - 10.8 * m.real + 3.9:
         return _D_upwards(z, N)
     else:
         return _D_downwards(z, N)
@@ -86,34 +86,34 @@ def _mie_An_Bn(m, x):
         summed is around 1e-6.
     """
 
-    nstop = int(x + 4.05 * x**0.33333 + 2.0)+1
+    nstop = int(x + 4.05 * x**0.33333 + 2.0) + 1
 
     if m.real > 0.0:
-        D = _D_calc(m, x, nstop+1)
+        D = _D_calc(m, x, nstop + 1)
 
-    a = np.zeros(nstop-1, dtype=complex)
-    b = np.zeros(nstop-1, dtype=complex)
+    a = np.zeros(nstop - 1, dtype=complex)
+    b = np.zeros(nstop - 1, dtype=complex)
 
     psi_nm1 = np.sin(x)                   # nm1 = n-1 = 0
-    psi_n   = psi_nm1/x - np.cos(x)       # n = 1
-    xi_nm1  = complex(psi_nm1, np.cos(x))
-    xi_n    = complex(psi_n,   np.cos(x)/x+np.sin(x))
+    psi_n = psi_nm1 / x - np.cos(x)       # n = 1
+    xi_nm1 = complex(psi_nm1, np.cos(x))
+    xi_n = complex(psi_n, np.cos(x) / x + np.sin(x))
 
     for n in range(1, nstop):
         if m.real == 0.0:
-            a[n-1] = (n*psi_n/x - psi_nm1) / (n*xi_n/x - xi_nm1)
-            b[n-1] = psi_n/xi_n
+            a[n - 1] = (n * psi_n / x - psi_nm1) / (n * xi_n / x - xi_nm1)
+            b[n - 1] = psi_n / xi_n
         else:
-            temp = D[n]/m+n/x
-            a[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
-            temp = D[n]*m+n/x
-            b[n-1] = (temp*psi_n-psi_nm1)/(temp*xi_n-xi_nm1)
+            temp = D[n] / m + n / x
+            a[n - 1] = (temp * psi_n - psi_nm1) / (temp * xi_n - xi_nm1)
+            temp = D[n] * m + n / x
+            b[n - 1] = (temp * psi_n - psi_nm1) / (temp * xi_n - xi_nm1)
 
-        xi      = (2*n+1)*xi_n/x - xi_nm1
-        xi_nm1  = xi_n
-        xi_n    = xi
+        xi = (2 * n + 1) * xi_n / x - xi_nm1
+        xi_nm1 = xi_n
+        xi_n = xi
         psi_nm1 = psi_n
-        psi_n   = xi_n.real
+        psi_n = xi_n.real
 
     return [a, b]
 
@@ -125,20 +125,22 @@ def _small_conducting_mie(m, x):
         as well as scattering asymmetry are calculated for small spheres
         (x<0.1) that are perfectly conducting (m.re == 0)
     """
-    ahat1 = complex(0, 2.0/3.0*(1-0.2*x**2))/complex(1-0.5*x**2, 2.0/3.0*x**3)
-    bhat1 = complex(0.0, (x**2-10.0)/30.0)/complex(1+0.5*x**2, -x**3/3.0)
-    ahat2 = complex(0.0, x**2/30.)
-    bhat2 = complex(0.0, -x**2/45.)
+    ahat1 = complex(0, 2.0 / 3.0 * (1 - 0.2 * x**2)) / \
+        complex(1 - 0.5 * x**2, 2.0 / 3.0 * x**3)
+    bhat1 = complex(0.0, (x**2 - 10.0) / 30.0) / \
+        complex(1 + 0.5 * x**2, -x**3 / 3.0)
+    ahat2 = complex(0.0, x**2 / 30.)
+    bhat2 = complex(0.0, -x**2 / 45.)
 
-    qsca = x**4*(6*abs(ahat1)**2 + 6*abs(bhat1)**2 + 10*abs(ahat2)**2 +
-                 10*abs(bhat2)**2)
+    qsca = x**4 * (6 * abs(ahat1)**2 + 6 * abs(bhat1)**2 + 10 * abs(ahat2)**2 +
+                   10 * abs(bhat2)**2)
     qext = qsca
-    g =  ahat1.imag*(ahat2.imag + bhat1.imag)
-    g += bhat2.imag*(5.0/9.0*ahat2.imag + bhat1.imag)
+    g = ahat1.imag * (ahat2.imag + bhat1.imag)
+    g += bhat2.imag * (5.0 / 9.0 * ahat2.imag + bhat1.imag)
     g += ahat1.real * bhat1.real
-    g *= 6*x**4/qsca
+    g *= 6 * x**4 / qsca
 
-    qback = 3*np.pi*x**4 * abs(ahat1-bhat1-5/3*(ahat2-bhat2))**2
+    qback = 3 * np.pi * x**4 * abs(ahat1 - bhat1 - 5 / 3 * (ahat2 - bhat2))**2
 
     return [qext, qsca, qback, g]
 
@@ -155,22 +157,24 @@ def _small_mie(m, x):
     x3 = x2 * x
     x4 = x2 * x2
 
-    D = m2 + 2 + (1-0.7*m2)*x2 - (8*m4-385*m2+350)*x4/1400.0 + \
-        2j*(m2-1)*x3*(1-0.1*x2)/3
-    ahat1 = 2j*(m2-1)/3*(1-0.1*x2+(4*m2+5)*x4/1400)/D
-    bhat1 = 1j*x2*(m2-1)/45 * (1+(2*m2-5)/70*x2)/(1-(2*m2-5)/30*x2)
-    ahat2 = 1j*x2*(m2-1)/15 * (1-x2/14)/(2*m2+3-(2*m2-7)/14*x2)
+    D = m2 + 2 + (1 - 0.7 * m2) * x2 - (8 * m4 - 385 * m2 + 350) * x4 / 1400.0 + \
+        2j * (m2 - 1) * x3 * (1 - 0.1 * x2) / 3
+    ahat1 = 2j * (m2 - 1) / 3 * (1 - 0.1 * x2 + (4 * m2 + 5) * x4 / 1400) / D
+    bhat1 = 1j * x2 * (m2 - 1) / 45 * (1 + (2 * m2 - 5) /
+                                       70 * x2) / (1 - (2 * m2 - 5) / 30 * x2)
+    ahat2 = 1j * x2 * (m2 - 1) / 15 * (1 - x2 / 14) / \
+        (2 * m2 + 3 - (2 * m2 - 7) / 14 * x2)
 
-    qext = 6*x*(ahat1+bhat1+5*ahat2/3).real
-    T = abs(ahat1)**2+abs(bhat1)**2+5/3*abs(ahat2)**2
-    temp = ahat2+bhat1
-    g = (ahat1*temp.conjugate()).real/T
+    qext = 6 * x * (ahat1 + bhat1 + 5 * ahat2 / 3).real
+    T = abs(ahat1)**2 + abs(bhat1)**2 + 5 / 3 * abs(ahat2)**2
+    temp = ahat2 + bhat1
+    g = (ahat1 * temp.conjugate()).real / T
 
     qsca = qext
     if m.imag < 0:
-        qsca = 6*x4*T
+        qsca = 6 * x4 * T
 
-    qback = 3*np.pi*x4*abs(ahat1-bhat1-5*ahat2/3)**2
+    qback = 3 * np.pi * x4 * abs(ahat1 - bhat1 - 5 * ahat2 / 3)**2
     return [qext, qsca, qback, g]
 
 
@@ -194,26 +198,26 @@ def _mie_scalar(m, x):
     a, b = _mie_An_Bn(m, x)
 
     nmax = len(a)
-    n    = np.arange(1, nmax+1)
-    cn   = 2.0*n + 1.0
-    x2   = x*x
+    n = np.arange(1, nmax + 1)
+    cn = 2.0 * n + 1.0
+    x2 = x * x
 
-    qext = 2*np.sum(cn * (a.real + b.real))/x2
+    qext = 2 * np.sum(cn * (a.real + b.real)) / x2
     qsca = qext
 
     if m.imag != 0:
-        qsca = 2*np.sum(cn*(abs(a)**2 + abs(b)**2))/x2
+        qsca = 2 * np.sum(cn * (abs(a)**2 + abs(b)**2)) / x2
 
-    qback = abs(np.sum((-1)**n * cn*(a - b)))**2/x2
+    qback = abs(np.sum((-1)**n * cn * (a - b)))**2 / x2
 
-    c1n  = n*(n+2)/(n+1)
-    c2n  = cn/n/(n+1)
+    c1n = n * (n + 2) / (n + 1)
+    c2n = cn / n / (n + 1)
     g = 0
-    for i in range(nmax-1):
-        asy1 = c1n[i] * (a[i] * a[i+1].conjugate() +
-                         b[i] * b[i+1].conjugate()).real
+    for i in range(nmax - 1):
+        asy1 = c1n[i] * (a[i] * a[i + 1].conjugate() +
+                         b[i] * b[i + 1].conjugate()).real
         asy2 = c2n[i] * (a[i] * b[i].conjugate()).real
-        g += 4*(asy1 + asy2)/qsca/x2
+        g += 4 * (asy1 + asy2) / qsca / x2
 
     return [qext, qsca, qback, g]
 
@@ -246,10 +250,10 @@ def mie(m, x):
         raise RuntimeError('m and x arrays to mie must be same length')
 
     thelen = max(xlen, mlen)
-    qext  = np.empty(thelen)
-    qsca  = np.empty(thelen)
+    qext = np.empty(thelen)
+    qsca = np.empty(thelen)
     qback = np.empty(thelen)
-    g     = np.empty(thelen)
+    g = np.empty(thelen)
 
     for i in range(thelen):
         if mlen > 0:
@@ -274,17 +278,19 @@ def _small_mie_conducting_S1_S2(m, x, mu):
        Returns S1 and S2 at each angle in the array mu.
     """
 
-    ahat1 = 2j/3*(1-0.2*x**2)/(1-0.5*x**2+2j/3*x**3)
-    bhat1 = 1j/3*(0.1*x**2-1)/(1+0.5*x**2-1j/3*x**3)
-    ahat2 = 1j/30*x**2
-    bhat2 = -1j*x**2/45
+    ahat1 = 2j / 3 * (1 - 0.2 * x**2) / (1 - 0.5 * x**2 + 2j / 3 * x**3)
+    bhat1 = 1j / 3 * (0.1 * x**2 - 1) / (1 + 0.5 * x**2 - 1j / 3 * x**3)
+    ahat2 = 1j / 30 * x**2
+    bhat2 = -1j * x**2 / 45
 
-    S1 = 1.5*x3*(ahat1 + bhat1*mu + 5/3*ahat2*mu + 5/3*bhat2*(2*mu**2-1))
-    S2 = 1.5*x3*(bhat1 + ahat1*mu + 5/3*bhat2*mu + 5/3*ahat2*(2*mu**2-1))
+    S1 = 1.5 * x**3 * (ahat1 + bhat1 * mu + 5 / 3 * ahat2 *
+                       mu + 5 / 3 * bhat2 * (2 * mu**2 - 1))
+    S2 = 1.5 * x**3 * (bhat1 + ahat1 * mu + 5 / 3 * bhat2 *
+                       mu + 5 / 3 * ahat2 * (2 * mu**2 - 1))
 
-    qext = x**4*(6*abs(ahat1)**2 + 6*abs(bhat1)**2 + 10*abs(ahat2)**2 +
-                 10*abs(bhat2)**2)
-    norm = np.sqrt(qext*np.pi*x**2)
+    qext = x**4 * (6 * abs(ahat1)**2 + 6 * abs(bhat1)**2 + 10 * abs(ahat2)**2 +
+                   10 * abs(bhat2)**2)
+    norm = np.sqrt(qext * np.pi * x**2)
     S1 /= norm
     S2 /= norm
 
@@ -306,17 +312,19 @@ def _small_mie_S1_S2(m, x, mu):
     x3 = x2 * x
     x4 = x2 * x2
 
-    D = m2 + 2 + (1-0.7*m2)*x2 - (8*m4-385*m2+350)*x4/1400.0 + \
-        2j*(m2-1)*x3*(1-0.1*x2)/3
-    ahat1 = 2j*(m2-1)/3*(1-0.1*x2+(4*m2+5)*x4/1400)/D
-    bhat1 = 1j*x2*(m2-1)/45 * (1+(2*m2-5)/70*x2)/(1-(2*m2-5)/30*x2)
-    ahat2 = 1j*x2*(m2-1)/15 * (1-x2/14)/(2*m2+3-(2*m2-7)/14*x2)
+    D = m2 + 2 + (1 - 0.7 * m2) * x2 - (8 * m4 - 385 * m2 + 350) * x4 / 1400.0 + \
+        2j * (m2 - 1) * x3 * (1 - 0.1 * x2) / 3
+    ahat1 = 2j * (m2 - 1) / 3 * (1 - 0.1 * x2 + (4 * m2 + 5) * x4 / 1400) / D
+    bhat1 = 1j * x2 * (m2 - 1) / 45 * (1 + (2 * m2 - 5) /
+                                       70 * x2) / (1 - (2 * m2 - 5) / 30 * x2)
+    ahat2 = 1j * x2 * (m2 - 1) / 15 * (1 - x2 / 14) / \
+        (2 * m2 + 3 - (2 * m2 - 7) / 14 * x2)
 
-    S1 = 1.5*x3*(ahat1 + bhat1*mu + 5/3*ahat2*mu)
-    S2 = 1.5*x3*(bhat1 + ahat1*mu + 5/3*ahat2*(2*mu**2-1))
+    S1 = 1.5 * x3 * (ahat1 + bhat1 * mu + 5 / 3 * ahat2 * mu)
+    S2 = 1.5 * x3 * (bhat1 + ahat1 * mu + 5 / 3 * ahat2 * (2 * mu**2 - 1))
 
     # norm = sqrt(qext*pi*x**2)
-    norm = np.sqrt(np.pi*6*x**3*(ahat1 + bhat1 + 5*ahat2/3).real)
+    norm = np.sqrt(np.pi * 6 * x**3 * (ahat1 + bhat1 + 5 * ahat2 / 3).real)
     S1 /= norm
     S2 /= norm
 
@@ -344,18 +352,19 @@ def mie_S1_S2(m, x, mu):
         pi_nm2 = 0
         pi_nm1 = 1
         for n in range(1, nstop):
-            tau_nm1 =  n * mu[k] * pi_nm1 - (n + 1) * pi_nm2
-            S1[k] += (2*n+1)*(pi_nm1 * a[n-1] + tau_nm1 * b[n-1])/(n+1)/n
-            S2[k] += (2*n+1)*(tau_nm1 * a[n-1] + pi_nm1 * b[n-1])/(n+1)/n
+            tau_nm1 = n * mu[k] * pi_nm1 - (n + 1) * pi_nm2
+            S1[k] += (2 * n + 1) * (pi_nm1 * a[n - 1] +
+                                    tau_nm1 * b[n - 1]) / (n + 1) / n
+            S2[k] += (2 * n + 1) * (tau_nm1 * a[n - 1] +
+                                    pi_nm1 * b[n - 1]) / (n + 1) / n
 
             temp = pi_nm1
-            pi_nm1 = ((2*n+1) * mu[k] * pi_nm1 - (n+1) * pi_nm2)/n
+            pi_nm1 = ((2 * n + 1) * mu[k] * pi_nm1 - (n + 1) * pi_nm2) / n
             pi_nm2 = temp
 
     # calculate norm = sqrt(pi * Qext * x**2)
-    nstop = len(a)
-    n    = np.arange(1, nstop+1)
-    norm = np.sqrt(2*np.pi*np.sum((2*n + 1) * (a.real + b.real)))
+    n = np.arange(1, nstop + 1)
+    norm = np.sqrt(2 * np.pi * np.sum((2 * n + 1) * (a.real + b.real)))
 
     S1 /= norm
     S2 /= norm
@@ -374,14 +383,14 @@ def mie_cdf(m, x, num):
     mu = np.linspace(-1, 1, num)
     s1, s2 = mie_S1_S2(m, x, mu)
 
-    s =  (abs(s1)**2+abs(s2)**2)/2
+    s = (abs(s1)**2 + abs(s2)**2) / 2
 
     cdf = np.zeros(num)
-    sum = 0
+    total = 0
     for i in range(num):
         # need the extra 2pi because scattering normalized over 4pi steradians
-        sum += s[i] * 2 * np.pi * (2/num)
-        cdf[i] = sum
+        total += s[i] * 2 * np.pi * (2 / num)
+        cdf[i] = total
 
     return mu, cdf
 
@@ -407,24 +416,24 @@ def mie_mu_with_uniform_cdf(m, x, num):
     cdf[0] = 0                       # initial cdf is zero
 
     big_k = 0                        # index into big_cdf
-    for k in range(1, num-1):
+    for k in range(1, num - 1):
 
-        target = k/(num-1)
+        target = k / (num - 1)
         while big_cdf[big_k] < target:
             big_k += 1
 
-        delta     = big_cdf[big_k] - target
-        delta_cdf = big_cdf[big_k] - big_cdf[big_k-1]
-        delta_mu  = big_mu[big_k] - big_mu[big_k-1]
+        delta = big_cdf[big_k] - target
+        delta_cdf = big_cdf[big_k] - big_cdf[big_k - 1]
+        delta_mu = big_mu[big_k] - big_mu[big_k - 1]
 
-        mu[k] = big_mu[big_k] - delta/delta_cdf*delta_mu   # interpolate
+        mu[k] = big_mu[big_k] - delta / delta_cdf * delta_mu   # interpolate
         cdf[k] = target
 
 #       print(' mu[',k,']=% .5f'%mu[k],' cdf[',k,']=% .5f'%cdf[k],
 #       'cdf=',big_cdf[big_k], fraction)
 
-    mu[num-1] = 1                    # cos[0 degrees] is 1
-    cdf[num-1] = 1                   # last cdf is one
+    mu[num - 1] = 1                    # cos[0 degrees] is 1
+    cdf[num - 1] = 1                   # last cdf is one
 
     return [mu, cdf]
 
@@ -439,59 +448,62 @@ def generate_mie_costheta(mu_cdf):
     returns the cosine of the scattering angle
     """
 
-    num = len(mu_cdf)-1
-    index = int(np.random.random_sample()*num)
+    num = len(mu_cdf) - 1
+    index = int(np.random.random() * num)
     if index >= num:
-        index = num-1
+        index = num - 1
 
-    x = mu_cdf[index]+(mu_cdf[index+1]-mu_cdf[index])*np.random.random_sample()
+    x = mu_cdf[index]
+    x += (mu_cdf[index + 1] - mu_cdf[index]) * np.random.random()
+
     return x
 
+
 def i_per(m, x, mu):
-    """Calculate the scattered intensity for a field perpendicular to the plane 
+    """Calculate the scattered intensity for a field perpendicular to the plane
        of scattering.  The intensity is normalized such that the integral of
-       the unpolarized intensity over 4pi steradians is equal to the single 
+       the unpolarized intensity over 4pi steradians is equal to the single
        scattering albedo.
-       
-       m is the complex index of refraction, 
+
+       m is the complex index of refraction,
        x is the size parameter
        mu is the cos(theta) of each direction desired
-       
+
        returns the intensity at each angle in the array mu.  Units [1/sr]
     """
-    s1, s2 = mie_S1_S2(m,x,mu)
+    s1, s2 = mie_S1_S2(m, x, mu)
     intensity = abs(s1)**2
     return intensity.astype('float')
 
 
 def i_par(m, x, mu):
-    """Calculate the scattered intensity for a field parallel to the plane 
+    """Calculate the scattered intensity for a field parallel to the plane
        of scattering.  The intensity is normalized such that the integral of
-       the unpolarized intensity over 4pi steradians is equal to the single 
+       the unpolarized intensity over 4pi steradians is equal to the single
        scattering albedo.
-       
-       m is the complex index of refraction, 
+
+       m is the complex index of refraction,
        x is the size parameter
        mu is the cos(theta) of each direction desired
-       
+
        returns the intensity at each angle in the array mu.  Units [1/sr]
     """
-    s1, s2 = mie_S1_S2(m,x,mu)
+    s1, s2 = mie_S1_S2(m, x, mu)
     intensity = abs(s2)**2
     return intensity.astype('float')
 
 
 def i_unpolarized(m, x, mu):
-    """Calculate the scattered intensity for unpolarized incident light.  
+    """Calculate the scattered intensity for unpolarized incident light.
        The intensity is normalized such that the integral over
        4pi steradians is equal to the single scattering albedo.
-       
-       m is the complex index of refraction, 
+
+       m is the complex index of refraction,
        x is the size parameter
        mu is the cos(theta) of each direction desired
-       
+
        returns the intensity at each angle in the array mu.  Units [1/sr]
     """
-    s1, s2 = mie_S1_S2(m,x,mu)
-    intensity = (abs(s1)**2 + abs(s2)**2)/2
+    s1, s2 = mie_S1_S2(m, x, mu)
+    intensity = (abs(s1)**2 + abs(s2)**2) / 2
     return intensity.astype('float')
