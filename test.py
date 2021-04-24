9c3f8f9efb3c7b6e36d99c52dc463f7a2ce69cd4
Scott Prahl <scott@prahls.com>
2021-04-24 14:55:59 -0700

    remove license and variable renaming

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 #! /usr/bin/env python3
 
-"""
-Copyright 2017-19 Scott Prahl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
 import unittest
 import numpy as np
 import miepython
@@ -441,11 +431,11 @@ class notebook_tests(unittest.TestCase):
         N=500
         m=1.0
         r=500                            # nm
-        lambdaa = np.linspace(300,800,N)  # also in nm
+        lambda0 = np.linspace(300,800,N)  # also in nm
 
         mwater = 4/3   # rough approximation
         mm = m/mwater
-        xx = 2*np.pi*r*mwater/lambdaa
+        xx = 2*np.pi*r*mwater/lambda0
 
         qext, qsca, qback, g = miepython.mie(mm,xx) 
 
