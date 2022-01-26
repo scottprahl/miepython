d8591950ffdced647cdc0a1cea0324e264278ca7
Scott Prahl <scott@prahls.com>
2022-01-26 11:22:18 -0800

    minor cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,6 @@ napoleon_use_param = False
 napoleon_use_ivar = False
 napoleon_include_private_with_doc = False
 napoleon_use_rtype = False
-napoleon_use_param = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,6 +53,7 @@ exclude_patterns = [
     '**.ipynb_checkpoints',
     'omlc.org/*', 
     'refractiveindex.info/*',
+    'adaptive_functioning.ipynb'
 ]
 
 # I execute the notebooks manually in advance. If notebooks test the code,
@@ -74,7 +74,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 html_scaled_image_link = False
 
