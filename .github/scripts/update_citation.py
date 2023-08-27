e3a200603ebf7bdcf5c412952158481c44d1d983
Scott Prahl <scott@prahls.com>
2023-08-27 13:37:36 -0700

    add auto-dating in CITATION

---
+++ .github/scripts/update_citation.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import yaml
+
+# Replace with your username and repo name
+USERNAME = "scottprahl"
+REPO = "miepython"
+
+# Fetch latest release date
+response = requests.get(f"https://api.github.com/repos/{USERNAME}/{REPO}/releases/latest")
+release_date = json.loads(response.text)["published_at"].split("T")[0]
+
+# Read the existing CITATION.cff file
+with open("CITATION.cff", "r") as f:
+    cff_data = yaml.safe_load(f)
+
+# Update the date-released field
+cff_data["date-released"] = release_date
+
+# Save the updated data back to CITATION.cff
+with open("CITATION.cff", "w") as f:
+    yaml.dump(cff_data, f)
