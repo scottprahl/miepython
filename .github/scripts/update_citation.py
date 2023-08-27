d73aac599c86b482a6987c99a91114686c5c9acb
Scott Prahl <scott@prahls.com>
2023-08-27 14:16:06 -0700

    add version info to citation

--- .github/scripts/update_citation.py
+++ .github/scripts/update_citation.py
@@ -9,17 +9,28 @@ REPO = "miepython"
 # Fetch latest release date
 response = requests.get(f"https://api.github.com/repos/{USERNAME}/{REPO}/releases/latest")
 release_date = json.loads(response.text)["published_at"].split("T")[0]
+version = release_info["tag_name"]
 
 # Read the existing CITATION.cff file
 with open("CITATION.cff", "r") as f:
     cff_data = yaml.safe_load(f)
 
+# Create a flag to track if any change is made
+changed = False
+
 # Update the date-released field only if it's different
 if cff_data.get("date-released") != release_date:
     cff_data["date-released"] = release_date
+    changed = True
 
-    # Save the updated data back to CITATION.cff
+# Update the version field only if it's different
+if cff_data.get("version") != version:
+    cff_data["version"] = version
+    changed = True
+
+# Save the updated data back to CITATION.cff only if there was a change
+if changed:
     with open("CITATION.cff", "w") as f:
         yaml.dump(cff_data, f)
 else:
-    print("No change in release date. No update needed.")
+    print("No change in release date or version. No update needed.")
