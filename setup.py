3320c219f9cac0022b101d53ebb3f58417a2bf33
Scott Prahl <scott@prahls.com>
2024-05-07 12:34:12 -0700

    set 3.7 as earliest python version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def get_contents(filename):
 
 setup(
     name=project,
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     long_description=get_contents('README.rst'),
     long_description_content_type='text/x-rst',
     version=get_init_property('__version__'),
