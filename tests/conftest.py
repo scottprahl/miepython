fed151f05c6bdd5d140f7ebc46fab14201d8faa2
Scott Prahl <scott@prahls.com>
2024-05-07 10:34:41 -0700

    add docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+"""Configuration file for pytest."""
+
+
 def pytest_addoption(parser):
+    """Add --notebooks option to pytest parser."""
     parser.addoption(
         "--notebooks", action="store_true", default=False, dest="notebooks",
         help="test notebooks by running them"
@@ -6,12 +10,14 @@ def pytest_addoption(parser):
 
 
 def pytest_collection_modifyitems(config, items):
+    """Modify files to automatically be tested by pytest."""
     import pytest
 
     # Skip items marked with `notebooks` by default
     if config.option.notebooks:
         # --notebooks was set, do not skip
         return
+
     skip_notebooks = pytest.mark.skip(reason="--notebooks option not used")
     for item in items:
         if "notebooks" in item.keywords:
