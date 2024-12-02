97cc7d086bd744d48140423253b82a083628c568
Scott Prahl <scott@prahls.com>
2024-12-02 15:53:14 -0800

    blacking

--- docs/conf.py
+++ docs/conf.py
@@ -15,49 +15,48 @@ docstrings that follow the Google docstring format.
 import re
 import os.path
 
-project = 'miepython'
+project = "miepython"
+
 
 def get_init_property(prop):
     """Return property from __init__.py."""
     here = os.path.abspath(os.path.dirname(__file__))
-    file_name = os.path.join(here, '..', project, '__init__.py')
+    file_name = os.path.join(here, "..", project, "__init__.py")
     regex = r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop)
-    with open(file_name, 'r', encoding='utf-8') as file:
+    with open(file_name, "r", encoding="utf-8") as file:
         result = re.search(regex, file.read())
     return result.group(1)
+
 
 release = get_init_property("__version__")
 author = get_init_property("__author__")
 copyright = get_init_property("__copyright__")
 
-master_doc = 'index'
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
 # Sphinx extension modules
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax',
-    'sphinx_automodapi.automodapi',
-    'nbsphinx',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinx_automodapi.automodapi",
+    "nbsphinx",
 ]
 numpydoc_show_class_members = False
 napoleon_use_param = False
 napoleon_use_rtype = False
 
 # List of patterns, relative to source directory, of files to ignore
-exclude_patterns = ['_build',
-                    '**.ipynb_checkpoints',
-                    'adaptive_functioning.ipynb'
-                    ]
+exclude_patterns = ["_build", "**.ipynb_checkpoints", "adaptive_functioning.ipynb"]
 
 # I execute the notebooks manually in advance.
-nbsphinx_execute = 'never'
+nbsphinx_execute = "never"
 nbsphinx_allow_errors = True
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_scaled_image_link = False
-html_sourcelink_suffix = ''
+html_sourcelink_suffix = ""
