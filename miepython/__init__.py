38ad4714c72755e8e3b0a9c4488800fcfd322e1a
Scott Prahl <scott@prahls.com>
2025-02-13 19:24:06 -0800

    simplify user importing

--- miepython/__init__.py
+++ miepython/__init__.py
@@ -11,39 +11,68 @@ The extinction efficiency, scattering efficiency, backscattering, and
 scattering asymmetry for a sphere with complex index of refraction m,
 diameter d, and wavelength lambda can be found by::
 
-    qext, qsca, qback, g = miepython.ez_mie(m, d, lambda0)
+    import miepython as mie
+    qext, qsca, qback, g = mie.ez_mie(m, d, lambda0)
 
 The normalized scattering values for angles mu=cos(theta) are::
 
-    Ipar, Iper = miepython.ez_intensities(m, d, lambda0, mu)
+    Ipar, Iper = mie.ez_intensities(m, d, lambda0, mu)
 
 If the size parameter is known, then use::
 
-    miepython.mie(m, x)
+    mie.mie(m, x)
 
 Mie scattering amplitudes S1 and S2 (complex numbers):
 
-    miepython.mie_S1_S2(m, x, mu)
+    mie.mie_S1_S2(m, x, mu)
 
 Normalized Mie scattering intensities for angles mu=cos(theta)::
 
-    miepython.i_per(m, x, mu)
-    miepython.i_par(m, x, mu)
-    miepython.i_unpolarized(m, x, mu)
+    mie.i_per(m, x, mu)
+    mie.i_par(m, x, mu)
+    mie.i_unpolarized(m, x, mu)
 
 Mie scattering intensities normalized to one when integrated over all angles::
 
-    miepython.i_per(m, x, mu, norm='one')
-    miepython.i_par(m, x, mu, norm='one')
-    miepython.i_unpolarized(m, x, mu, norm='one')
+    mie.i_per(m, x, mu, norm='one')
+    mie.i_par(m, x, mu, norm='one')
+    mie.i_unpolarized(m, x, mu, norm='one')
 
 The scattering matrix
 
-    miepython.mie_phase_matrix(m, x, mu)
+    mie.mie_phase_matrix(m, x, mu)
 
 """
 
-__version__ = "2.6.0"
+import os
+
+USE_JIT = os.environ.get("MIEPYTHON_USE_JIT", "1").lower() == "1"
+
+if USE_JIT:
+    from .mie_jit import _an_bn, _cn_dn, _mie_S1_S2
+
+else:
+    from .mie_nojit import _an_bn, _cn_dn, _mie_S1_S2
+
+from .main import ez_mie, ez_intensities, i_par, i_per, i_unpolarized
+from .main import mie, mie_S1_S2, mie_phase_matrix, mie_coefficients, an_bn, cn_dn
+
+# The only functions exposed to the user
+__all__ = (
+    "ez_mie",
+    "ez_intensities",
+    "i_par",
+    "i_per",
+    "i_unpolarized",
+    "mie",
+    "mie_phase_matrix",
+    "mie_coefficients",
+    "an_bn",
+    "cn_dn",
+    "mie_S1_S2",
+)
+
+__version__ = "3.0.0"
 __author__ = "Scott Prahl"
 __email__ = "scott.prahl@oit.edu"
 __copyright__ = "2017-25, Scott Prahl"
